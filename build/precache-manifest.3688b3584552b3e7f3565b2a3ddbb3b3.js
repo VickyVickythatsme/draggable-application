195efb89f0451ea71542b9733db578733494dc52
vicky <vickywang199688@gmail.com>
2021-09-20 02:03:42 +1000

    vicky-commit

---
+++ build/precache-manifest.3688b3584552b3e7f3565b2a3ddbb3b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "511ebf13bfcaadac6e68778fbf57b96b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ecdc951c4a0185c2cba",
+    "url": "/static/css/main.4bca4a9a.chunk.css"
+  },
+  {
+    "revision": "eb9c9c473d48d42433de",
+    "url": "/static/js/2.91939f43.chunk.js"
+  },
+  {
+    "revision": "0ecdc951c4a0185c2cba",
+    "url": "/static/js/main.3c415fee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
